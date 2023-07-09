10cfa3bf2dc8d000e8957ea26ec354fd00459889
root <mubashir.zamir@goodcoresoft.com>
2023-07-09 20:07:25 +0500

    #main: fix for api not working on prod attempt 3

--- src/app/api/spellcheck/route.ts
+++ src/app/api/spellcheck/route.ts
@@ -1,8 +1,11 @@
 import Typo from "typo-js";
 import {NextRequest, NextResponse} from "next/server";
+import path from 'path';
+
+const directory = path.join(process.cwd(), 'dictionaries');
 
 const dictionary = new Typo('en_US', null, null, {
-    dictionaryPath: 'src/app/api/spellcheck/dictionaries'
+    dictionaryPath: directory
 })
 
 const isValidWord = (word: string): boolean => dictionary.check(word)
